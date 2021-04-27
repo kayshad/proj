0cc2e891d12e92a35104eb56da1743bd92c7ac3d
kayshad <kaisslama@gmail.com>
2021-04-27 19:35:23 +0200

    d3d

--- c.py
+++ c.py
@@ -1,8 +1,7 @@
 from manim import *
 
 
-from manim import *
-class MaScene(Scene):
+class MaScene(ThreeDScene):
     def construct(self):
         l = NumberLine(include_numbers=True)
         l1 = NumberLine(include_numbers=True, unit_size=2)
@@ -17,4 +16,60 @@ class MaScene(Scene):
         b2text = b2.get_tex("x-x_1")
         self.add(line, dot, dot2, b1, b2, b1text, b2text)
         self.play(Transform(l, l1),run_time=3)
+        
+        axes = ThreeDAxes()
+        sphere = ParametricSurface(
+            lambda u, v: np.array([
+                1.5 * np.cos(u) * np.cos(v),
+                1.5 * np.cos(u) * np.sin(v),
+                1.5 * np.sin(u)
+            ]), v_min=0, v_max=TAU, u_min=-PI / 2, u_max=PI / 2,
+            checkerboard_colors=[RED_D, RED_E], resolution=(15, 32)
+        )
+        self.renderer.camera.light_source.move_to(3*IN) # changes the source of the light
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.add(axes, sphere)
         self.wait(10)
+
+class MovingFrameBox(Scene):
+    def construct(self):
+        text=MathTex(
+            "\\frac{d}{dx}f(x)g(x)=","f(x)\\frac{d}{dx}g(x)","+",
+            "g(x)\\frac{d}{dx}f(x)"
+        )
+        self.play(Write(text))
+        framebox1 = SurroundingRectangle(text[1], buff = .1)
+        framebox2 = SurroundingRectangle(text[3], buff = .1)
+        self.play(
+            Create(framebox1),
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox1,framebox2),
+        )
+        self.wait()
+
+class FixedInFrameMObjectTest(ThreeDScene):
+    def construct(self):
+        axes = ThreeDAxes()
+        self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
+        text3d = Text("This is a 3D text")
+        self.add_fixed_in_frame_mobjects(text3d)
+        text3d.to_corner(UL)
+        self.add(axes)
+        self.wait()
+
+class ThreeDLightSourcePosition(ThreeDScene):
+    def construct(self):
+        axes = ThreeDAxes()
+        sphere = ParametricSurface(
+            lambda u, v: np.array([
+                1.5 * np.cos(u) * np.cos(v),
+                1.5 * np.cos(u) * np.sin(v),
+                1.5 * np.sin(u)
+            ]), v_min=0, v_max=TAU, u_min=-PI / 2, u_max=PI / 2,
+            checkerboard_colors=[RED_D, RED_E], resolution=(15, 32)
+        )
+        self.renderer.camera.light_source.move_to(3*IN) # changes the source of the light
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.add(axes, sphere)
