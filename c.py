c6ad40843d321c646ee08f2b8f55dc01b2071904
kayshad <kaisslama@gmail.com>
2021-04-27 18:53:11 +0200

    resolu

--- c.py
+++ c.py
@@ -4,6 +4,17 @@ from manim import *
 from manim import *
 class MaScene(Scene):
     def construct(self):
-        l = NumberLine()
+        l = NumberLine(include_numbers=True)
+        l1 = NumberLine(include_numbers=True, unit_size=2)
         self.add(l)
         self.wait()
+        dot = Dot([-1, 0, 0]).shift(2*UP)
+        dot2 = Dot([1, 0, 0]).shift(2*UP)
+        line = Line(dot.get_center(), dot2.get_center()).set_color(ORANGE)
+        b1 = Brace(line)
+        b1text = b1.get_text("Horizontal distance")
+        b2 = Brace(line, direction=line.copy().rotate(PI / 2).get_unit_vector())
+        b2text = b2.get_tex("x-x_1")
+        self.add(line, dot, dot2, b1, b2, b1text, b2text)
+        self.play(Transform(l, l1),run_time=3)
+        self.wait(10)
